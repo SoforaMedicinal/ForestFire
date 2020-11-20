87eeb60cb5d8dabe7f61c1d49387fe9b9e6f77fc
Липчанская Анна <44881547+SoforaMedicinal@users.noreply.github.com>
2020-11-20 20:00:16 +0200

    Add files via upload

---
+++ ForestFireModel/ForestFireModel/ForestModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestFireModel
+{
+    public class ForestModel
+    {
+        public enum Landscape : byte
+        {
+            Empty = 0,
+            Tree = 1,
+            Fire = 2
+        }
+
+        private Landscape[,] _matrix;
+
+        public Landscape[,] GetState()
+        {
+            return _matrix;
+        }
+
+        private static readonly Random rand = new Random();
+
+        private float _forestFireChance { get; set; }
+
+        private float _treeGrowChance { get; set; }
+
+        public int HeightMap { get; set; }
+
+        public int WidthMap { get; set; }
+
+        protected ForestModel(float f, float p, int height, int width)
+        {
+            _forestFireChance = f;
+            _treeGrowChance = p;
+            HeightMap = height;
+            WidthMap = width;
+
+            Initialize();
+        }
+
+        public static Result<ForestModel> Create(float f, float p, int height, int width)
+        {
+            var errors = new List<string>();
+
+            if (!(0 <= f && f <= 1)) errors.Add("Вероятность возгорания дерева только [0,1]");
+            if (!(0 <= p && p <= 1)) errors.Add("Вероятность заполнением дерева только [0,1]");
+            if (height < 1) errors.Add("Высота не может быть меньше единицы");
+            if (width < 1) errors.Add("Ширина не может быть меньше единицы");
+
+            if (errors.Any())
+            {
+                return Result<ForestModel>.Fail(errors);
+            }
+
+            return Result<ForestModel>.Success(new ForestModel(f, p, height, width));
+        }
+
+        private void Initialize()
+        {
+            _matrix = new Landscape[HeightMap, WidthMap];
+            _matrix.Initialize();
+        }
+
+        public Landscape[,] Step()
+        {
+            var newForest = (Landscape[,])_matrix.Clone();
+
+            int numRows = _matrix.GetLength(0);
+            int numCols = _matrix.GetLength(1);
+
+            for (int x = 1; x < numRows - 1; x++)
+            {
+                for (int y = 1; y < numCols - 1; y++)
+                {
+                    switch (_matrix[x, y])
+                    {
+                        case Landscape.Empty:
+                            if (rand.NextDouble() < _treeGrowChance)
+                                newForest[x, y] = Landscape.Tree;
+                            break;
+
+                        case Landscape.Tree:
+                            if (NeighborLandscape(_matrix, x, y, Landscape.Fire) || rand.NextDouble() < _forestFireChance)
+                                newForest[x, y] = Landscape.Fire;
+                            break;
+
+                        case Landscape.Fire:
+                            newForest[x, y] = Landscape.Empty;
+                            break;
+                    }
+                }
+            }
+
+            _matrix = newForest;
+            return newForest;
+        }
+
+        private bool NeighborLandscape(Landscape[,] state, int x, int y, Landscape value)
+        {
+            for (int r = -1; r <= 1; r++)
+            {
+                for (int c = -1; c <= 1; c++)
+                {
+                    if (r == 0 && c == 0)
+                        continue;
+
+                    if (state[x + r, y + c] == value)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ForestFireModel/ForestFireModel/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static ForestFireModel.ForestModel;
+
+namespace ForestFireModel
+{
+    class Program
+    {
+        public static float f;
+        public static float p;
+        public static int height;
+        public static int width;
+        public static int interval;
+
+        private static Timer timer;
+        private Encoding ascii = Encoding.ASCII;
+        static void Main(string[] args)
+        {
+            var parser = ParserValue();
+            if (!parser.Succeeded)
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, parser.Errors));
+                Console.ReadLine();
+                return;
+            }
+
+            ForestModel model;
+            var createResult = ForestModel.Create(f, p, height, width);
+            if (!createResult.Succeeded)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, createResult.Errors));
+            }
+            else
+            {
+                Console.OutputEncoding = System.Text.Encoding.GetEncoding(1252);
+                model = createResult.Value;
+                timer = new Timer(new TimerCallback(Drawing), model, 0, interval);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static Result<bool> ParserValue()
+        {
+            IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+
+            var errors = new List<string>();
+
+            if (!float.TryParse(ConfigurationManager.AppSettings["WoodFireRisk"], NumberStyles.Float, formatter, out f)) { errors.Add("Поле WoodFireRisk не удалось преобразовать в float"); }
+            if (!float.TryParse(ConfigurationManager.AppSettings["FillingWithWood"],NumberStyles.Float,formatter,out p)) { errors.Add("Поле FillingWithWood  не удалось преобразовать в float"); }
+            if (!int.TryParse(ConfigurationManager.AppSettings["HeightMap"],out height)) { errors.Add("Поле HeightMap не удалось преобразовать в int"); }
+            if (!int.TryParse(ConfigurationManager.AppSettings["WidthMap"], out width)) { errors.Add("Поле WidthMap не удалось преобразовать в int"); }
+            if (!int.TryParse(ConfigurationManager.AppSettings["Interval"], out interval)) { errors.Add("Поле Interval не удалось преобразовать в int"); }
+
+            if (errors.Any())
+            {
+                return Result<bool>.Fail(errors);
+            }
+
+            return Result<bool>.Success(true);
+        }
+
+        public static void Drawing(Object model)
+        {
+            ForestModel forest = (ForestModel)model;
+            Console.Clear();
+
+            for (int y = 0; y < forest.HeightMap; y++)
+            {
+                Console.WriteLine("");
+                for (int x = 0; x < forest.WidthMap; x++)
+                {
+                    switch (forest.GetState()[y, x])
+                    {
+                        //Пустое поле
+                        case Landscape.Empty:
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.Write($"   ");
+                            break;
+
+                        //Поле с деревом
+                        case Landscape.Tree:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write($"•  ");
+                            break;
+
+                        //Поле с горящим деревом
+                        case Landscape.Fire:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write($"+  ");
+                            break;
+                    }
+                }
+            }
+
+            forest.Step();
+
+        }
+    }
+}
